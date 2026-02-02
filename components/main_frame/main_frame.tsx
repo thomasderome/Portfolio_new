7f348f1c6228f03dd59498f30db28686b4e20fb6
Thomas Derome <thomas.derome@epitech.eu>
2026-02-02 11:32:17 +0100

    [~] Change emplacement Selector and Button

--- components/main_frame/main_frame.tsx
+++ components/main_frame/main_frame.tsx
@@ -1,7 +1,9 @@
 "use client";
-import * as React from "react";
+import React, {ComponentType} from "react";
+
 import { Pages } from "./page_data";
-import {ComponentType} from "react";
+import Button from "@/components/element/button";
+import Selector from "@/components/element/selector";
 
 export default function Main_Frame() {
     const [Page_render, set_page_render] = React.useState<ComponentType >(() => Pages[0].element);
@@ -23,11 +25,11 @@ export default function Main_Frame() {
                 <Selector>
                     {Pages.map((menu) => {
                         return (
-                            <Button_select key={menu.name}
+                            <Button key={menu.name}
                                            onClick={() => set_page_render(() => menu.element)}
                             >
                                 {menu.name}
-                            </Button_select>
+                            </Button>
                         );
                     })}
                 </Selector>
@@ -36,22 +38,5 @@ export default function Main_Frame() {
                 </div>
             </div>
         </div>
-    );
-}
-
-function Selector( { children } : Readonly<{ children: React.ReactNode; }>) {
-    return (
-        <nav className="flex w-40 h-100 ml-3 mt-1 border-r border-(--color-second) mb-2">
-            {children}
-        </nav>
-    );
-}
-
-function Button_select( { children,  onClick} : Readonly<{ children: React.ReactNode; onClick: () => void; }>) {
-    return (
-        <button className="hover:bg-(--color-font) hover:text-black h-10 mr-2 w-full"
-                onClick={onClick}>
-            {children}
-        </button>
     );
 }
