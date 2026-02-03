afb150d8a9d4b3c4d5bb3177e0d111f2a439ebd0
Thomas Derome <thomas.derome@epitech.eu>
2026-02-03 12:02:37 +0100

    [~] Update button with merge classname

--- components/element/button.tsx
+++ components/element/button.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import cn from "@/components/element/tailwind_merge"
 
 interface Button_interface extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export default function Button_select({ children, ...props }: Button_interface) {
+export default function Button_select({ children, className, ...props }: Button_interface) {
   return (
-    <button className="mr-2 h-10 w-full hover:bg-(--color-font) hover:text-black" {...props}>
+    <button className={cn(`mr-2 h-10 w-full hover:bg-(--color-font) hover:text-black`, className)} {...props}>
       {children}
     </button>
   );
