9902600d9692852611d30f239c8cc93c5e79b238
Thomas Derome <thomas.derome@epitech.eu>
2026-02-03 12:05:43 +0100

    [+] Add merge class system

--- components/element/selector.tsx
+++ components/element/selector.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import cn from "@/components/element/tailwind_merge"
 
 interface Selector_interface extends React.HTMLProps<HTMLDivElement> {
   children: React.ReactNode;
 }
 
-export default function Selector({ children, ...props }: Selector_interface) {
+export default function Selector({ children, className, ...props }: Selector_interface) {
   return (
-    <nav className="mt-1 mb-2 ml-3 flex h-100 w-40 border-r border-(--color-second)" {...props}>
+    <nav className={cn("mt-1 mb-2 ml-3 flex flex-col h-100 w-40 border-r border-(--color-second)", className)} {...props}>
       {children}
     </nav>
   );
