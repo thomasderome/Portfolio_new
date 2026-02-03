7502774aaa26fafd2405517e16323165f25be30d
Thomas Derome <thomas.derome@epitech.eu>
2026-02-03 12:05:21 +0100

    [+] Add page of about menu

--- components/main_frame/page_data.ts
+++ components/main_frame/page_data.ts
@@ -1,9 +1,14 @@
 import interface_page from './interface_main_frame';
-import about_me from '@/components/page/about_me/about_me';
+import about_me from '@/components/page/about_me/page';
+import project from '@/components/page/project/project';
 
 export const Pages: interface_page[] = [
   {
     name: 'About-me',
     element: about_me,
   },
+  {
+    name: 'Project',
+    element: project
+  }
 ];
---
+++ components/page/about_me/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import About_card from "./about_me";
+
+export default function Page() {
+  return (
+    <div>
+      <About_card />
+    </div>
+  )
+}
