c66299f3d6d62f074c51f2dec2efc4d9265bfbf1
Thomas Derome <thomas.derome@epitech.eu>
2026-02-03 19:58:48 +0100

    [+] Add skill

--- components/page/about_me/page.tsx
+++ components/page/about_me/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import About_card from "./about_me";
+import About_card from './about_me';
+import Skill from './skill';
 
 export default function Page() {
   return (
-    <div>
+    <div className={'flex flex-1'}>
       <About_card />
+      <Skill />
     </div>
-  )
+  );
 }
