3899f582df8fda9d79172712d206f5316e7ce808
Thomas Derome <thomas.derome@epitech.eu>
2026-02-05 19:21:54 +0100

    [~] Change for best responsive

--- data/file/about_me.tsx
+++ data/file/about_me.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function about_me() {
   const birthDate = new Date('09/02/2007');
   const now = new Date();
@@ -10,15 +12,18 @@ export default function about_me() {
     age--;
 
   return (
-    <pre className={'text-base'}>
-      {`╔═══════════════════════════════════════════╗
-║ > NAME         : Thomas                   ║
-║ > AGES         : ${age} ans                   ║
-║ > LOCALISATION : Nice, France             ║
-║ > STATUS       : Student @ Epitech Nice   ║
-║ > PASSION      : Programming, video game  ║
-║                  skiing, bike             ║
-╚═══════════════════════════════════════════╝`}
-  </pre>
-)
+    <div className={'max-w-170 flex flex-col'}>
+      <h1 className={'text-2xl font-bold'}>
+        &gt; Thomas_Derome<span className={'blink text-xl'}>█</span>
+      </h1>
+      <div>
+        <span className={'text-sm'}>
+          I have always been passionate about computers, and I started programming by working on
+          various projects in different languages. Over the years, I have been able to develop and
+          improve my technical skills through self-taught passion. Now at Epitech, which fits in
+          with my learning philosophy. I am curious, enjoy working in a team, and love to learn.
+        </span>
+      </div>
+    </div>
+  );
 }
