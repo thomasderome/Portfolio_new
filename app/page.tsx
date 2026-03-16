c892edeed0cd52a2e72ccc68e9fec1e09e916946
Thomas Derome <thomas.derome@epitech.eu>
2026-03-16 17:07:41 +0100

    [~] add Suspense for build on main_frame

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import Main_Frame from '@/components/main_frame/main_frame';
+import { Suspense } from 'react';
 
 export default function Home() {
-    return <Main_Frame />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Main_Frame />
+        </Suspense>
+    );
 }
