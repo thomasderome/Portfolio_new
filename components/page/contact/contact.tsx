464add1671d5feb94525f6783cac587bc082861a
Thomas Derome <thomas.derome@epitech.eu>
2026-04-24 17:13:03 +0200

    [+] Contact me

--- components/page/contact/contact.tsx
+++ components/page/contact/contact.tsx
@@ -1,53 +1,78 @@
 'use client';
-import React, { ComponentType } from 'react';
-import { useIsMobile } from '@/hooks/use-mobile';
+import React from 'react';
 
 import Button from '@/components/element/button';
-import Selector from '@/components/element/selector';
-import { Pages } from '@/data/page_data';
+import Card from '@/components/element/card';
+import TextType from '@/components/effects/TextType';
+import { useGetTranslation } from '@/hooks/translation';
 
 export default function Contact_page() {
-    const isMobile = useIsMobile();
+    const translation = useGetTranslation();
 
     return (
-        <div className="cathodic_effect mr-2 ml-2 flex max-w-[140vh] flex-col text-xl text-(--color-font) after:pointer-events-none after:absolute after:inset-0 after:content-['']">
-            <div className="flex">
-                <span>╔</span>
-                <span>═══</span>
-                <span className="pr-1 pl-1">Portfolio</span>
-                <span className="max-w-217 overflow-hidden">
-                    ═══════════════════════════════════════════════════════════════════════════════════════════════════
-                </span>
-                <span>╗</span>
-            </div>
-            <div className={`mr-1 ml-1 max-w-259 bg-(--bg-terminal) ${isMobile ? '' : 'flex'}`}>
-                <div
-                    className={`mt-1 ml-3 flex border-(--color-second) ${isMobile ? 'flex-col overflow-x-scroll overflow-y-hidden' : 'mb-2 max-h-100 max-w-40 flex-col'}`}
-                >
-                    <form action="https://formspree.io/f/xojnyvza" method="POST">
-                        <label>
-                            <span>Your email:</span>
-                            <input type="email" name="email" required />
-                        </label>
-                        <label>
-                            Your message:
-                            <textarea
-                                name="message"
-                                className={'h-72 w-150 resize-none rounded-md bg-green-900'}
-                                required
-                            ></textarea>
-                        </label>
-                        <Button type="submit">Send</Button>
-                    </form>
+        <Card className="m-5 flex flex-col gap-4">
+            <div>
+                <div className="flex flex-col">
+                    <h1 className="text-xs text-(--color-font)">/home/thomas/contact_form.sh</h1>
+                    <div>
+                        &gt;{' '}
+                        <TextType
+                            text={[translation?.contact.contact]}
+                            typingSpeed={100}
+                            pauseDuration={5000}
+                            showCursor
+                            cursorCharacter="█"
+                            deletingSpeed={50}
+                            cursorBlinkDuration={0.75}
+                            loop={false}
+                            className='font-[--default-mono-font-family, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace]'
+                        />
+                    </div>
                 </div>
+
+                <form
+                    action="https://formspree.io/f/xojnyvza"
+                    method="POST"
+                    className="mt-4 flex flex-col gap-6"
+                >
+                    <div className="flex flex-col gap-2">
+                        <div className="flex items-center gap-2 border-b border-(--color-second) pb-1 focus-within:border-(--color-font)">
+                            <span className="text-(--color-font) opacity-50">&gt;</span>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                required
+                                placeholder={translation?.contact.email + '@email.com'}
+                                className="w-full bg-transparent py-1 text-(--color-font) outline-none"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <div className="flex flex-col border border-(--color-second) focus-within:border-(--color-font)">
+                            <textarea
+                                id="message"
+                                name="message"
+                                required
+                                placeholder="Message:"
+                                rows={8}
+                                className="w-full resize-none bg-transparent p-3 text-(--color-font) outline-none"
+                            ></textarea>
+                        </div>
+                    </div>
+
+                    <div className="mt-2 flex items-center gap-4">
+                        <Button
+                            type="submit"
+                            className="max-w-35 border border-(--color-second) uppercase transition-all hover:bg-(--color-font) hover:text-black"
+                        >
+                            [ {translation?.contact.send} ]
+                        </Button>
+                        <div className="hidden flex-1 border-t border-dashed border-(--color-second) opacity-30 md:block"></div>
+                    </div>
+                </form>
             </div>
-            <div className={'flex'}>
-                <span>╚</span>
-                <span className="max-w-255 overflow-hidden">
-                    ═══════════════════════════════════════════════════════════════════════════════════════════════════
-                </span>
-                <span>╝</span>
-            </div>
-        </div>
+        </Card>
     );
 }
--- data/page_data.ts
+++ data/page_data.ts
@@ -1,14 +1,19 @@
 import interface_page from '@/components/main_frame/interface_main_frame';
 import about_me from '@/components/page/about_me/page';
 import project from '@/components/page/project/project';
+import contact from '@/components/page/contact/contact';
 
 export const Pages: interface_page[] = [
     {
         name: 'about_me',
-        element: about_me,
+        page: about_me,
     },
     {
         name: 'project',
-        element: project,
+        page: project,
     },
+    {
+        name: 'contact',
+        page: contact
+    }
 ];
