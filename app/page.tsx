b2dfcdf975ec70f23113dd90a9951854094f93cb
Thomas Derome <thomas.derome@epitech.eu>
2026-04-24 15:49:18 +0200

    [~] fix center for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,20 @@
 
 import Main_Frame from '@/components/main_frame/main_frame';
 import { Suspense } from 'react';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 export default function Home() {
+    const mobile = useIsMobile();
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            
+            <div
+                className={
+                    mobile
+                        ? ''
+                        : 'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'
+                }
+            >
                 <Main_Frame />
             </div>
         </Suspense>
