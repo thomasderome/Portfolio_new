c9cbab1d1943f7641e9af095f019e5f4f1582dc1
thomasderome <thomas.derome@epitech.eu>
2026-05-24 14:08:41 +0200

    [+] Button lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,26 @@
+'use client'
+
 import './globals.css';
+import LanguageSwitcher from '@/components/element/LanguageSwitcher';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const ismobile = useIsMobile();
     return (
-        <html lang="en" className="font-[--font-mono]">
-            <body className="antialiased">{children}</body>
+        <html lang="fr" className="font-[--font-mono]">
+            <head>
+                <title>Thomas DEROME</title>
+            </head>
+            <body className="antialiased">
+                <div className={`fixed z-50 ${ismobile ? 'top-1 right-0' : 'top-4 right-3'}`}>
+                    <LanguageSwitcher />
+                </div>
+                {children}
+            </body>
         </html>
     );
 }
